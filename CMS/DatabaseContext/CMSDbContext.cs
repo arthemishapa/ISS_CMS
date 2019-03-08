e2a5c81a5c60b7c219279c605eb4831e742982e6
Isabela Fitero <isabelafitero@gmail.com>
2019-03-08 17:52:39 +0200

    MVC project.

---
+++ CMS/DatabaseContext/CMSDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+using CMS.Models.Entities;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace CMS.DatabaseContext
+{
+    public class CMSDbContext : IdentityDbContext<User>
+    {
+        public CMSDbContext()
+            : base("CMSDatabase", throwIfV1Schema: false)
+        {
+        }
+
+        public static CMSDbContext Create()
+        {
+            return new CMSDbContext();
+        }
+    }
+}
