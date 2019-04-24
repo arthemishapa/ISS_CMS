dc7ec92089e27169f2e3a3324d9dae8836f02007
dex4 <cata_dc98@yahoo.com>
2019-04-24 09:44:25 +0300

    Add UpdateConferenceViewModel

---
+++ CMS/CMS.Common/ViewModels/UpdateConferenceViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+using CMS.CMS.Common.Validation;
+
+namespace CMS.CMS.Common.ViewModels
+{
+    public class UpdateConferenceViewModel
+    {
+        [Required]
+        [Display(Name = "Conference name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Start date")]
+        [UIHint("DateTime")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MMMM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        [Display(Name = "End date")]
+        [UIHint("DateTime")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MMMM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        [ValidDate(StartDatePropertyName = "StartDate")]
+        public DateTime EndDate { get; set; }
+
+        [Required]
+        [Display(Name = "Abstract papers' deadline")]
+        [UIHint("DateTime")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MMMM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        [ValidDate(StartDatePropertyName = "StartDate", EndDatePropertyName = "BiddingDeadline")]
+        public DateTime AbstractPaperDeadline { get; set; }
+
+        [Required]
+        [Display(Name = "Proposals deadline")]
+        [UIHint("DateTime")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MMMM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        [ValidDate(StartDatePropertyName = "StartDate", EndDatePropertyName = "BiddingDeadline")]
+        public DateTime ProposalPaperDeadline { get; set; }
+
+        [Required]
+        [Display(Name = "Bidding deadline")]
+        [UIHint("DateTime")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MMMM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        [ValidDate(StartDatePropertyName = "StartDate", EndDatePropertyName = "EndDate")]
+        public DateTime BiddingDeadline { get; set; }
+    }
+}
